60542e7b74a1efda254656e911041584f5fb69b1
KojimaManten <jumper.manten@icloud.com>
2020-06-22 22:21:35 +0900

    TableViewを実装した。

--- MakeTableView/ViewController.swift
+++ MakeTableView/ViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
-
+    @IBOutlet weak var tableView: UITableView!
+    
 }
 
