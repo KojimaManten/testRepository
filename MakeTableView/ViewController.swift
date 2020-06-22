33a624fd6f2b0dc896bb2b55c98be1fc07854db0
KojimaManten <jumper.manten@icloud.com>
2020-06-22 22:10:25 +0900

    Initial Commit

---
+++ MakeTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MakeTableView
+//
+//  Created by 小島満天 on 2020/06/22.
+//  Copyright © 2020 Manchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
